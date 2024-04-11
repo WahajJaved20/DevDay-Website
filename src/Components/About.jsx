e861637edab1e50a3029df741c5d34d9ef34de86
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2024-04-11 15:06:18 +0500

    aboutUsResolved

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -40,7 +40,7 @@ function About(){
           <img src={aboutTwo} alt="" className="mx-auto lg:mx-0" />
           <div className="w-full lg:w-2/5">
                 <h1 className="font-Gotham font-extrabold text-headerColorOne text-6xl mb-8 text-center lg:text-left mt-20 lg:mt-0 lg:text-7xl">EX-COM</h1>
-                <p className="text-black font-circularStd font-semibold text-lg text-left lg:text-lg xl:text-lg">Leadership is an action it's no position. Those in action lead the way towards success, having zestful and dynamic leaders who know how to accelerate their way up in their game; and not only this, they know how to trump together. Welcoming the quintessential Executive Committee of ACM NUCES for the tenure of 2023-2024. We hope they touch new horizons and leave the remarkable footprints for others to follow.</p>
+                <p className="text-black font-circularStd mx-4 font-semibold text-lg text-left lg:text-lg xl:text-lg">Leadership is an action it's no position. Those in action lead the way towards success, having zestful and dynamic leaders who know how to accelerate their way up in their game; and not only this, they know how to trump together. Welcoming the quintessential Executive Committee of ACM NUCES for the tenure of 2023-2024. We hope they touch new horizons and leave the remarkable footprints for others to follow.</p>
             </div>
         </div>
         <div style={{
@@ -65,7 +65,7 @@ function About(){
           <img src={aboutFour} alt="" className="mx-auto lg:mx-0" />
           <div className="w-full lg:w-2/5">
                 <h1 className="font-Gotham text-headerColorOne font-extrabold text-6xl text-600 mb-8 text-center lg:text-left mt-20 lg:mt-0 lg:text-7xl">CORE TEAM</h1>
-                <p className="text-black font-circularStd font-semibold text-lg lg:text-lg xl:text-lg">In the realm of ACM DevDay, leadership transcends titles to embody proactive commitment. The Core Team for 2024 exemplifies dynamic leadership and collaborative prowess. As architects of innovation, we anticipate their transformative impact, leaving an enduring legacy for future initiatives. Here's to a remarkable tenure, inspiring others to follow suit.</p>
+                <p className="text-black font-circularStd font-semibold mx-4 text-lg lg:text-lg xl:text-lg">In the realm of ACM DevDay, leadership transcends titles to embody proactive commitment. The Core Team for 2024 exemplifies dynamic leadership and collaborative prowess. As architects of innovation, we anticipate their transformative impact, leaving an enduring legacy for future initiatives. Here's to a remarkable tenure, inspiring others to follow suit.</p>
             </div>
         </div>
 
@@ -82,7 +82,7 @@ function About(){
           <img src={aboutFive} alt="" className="mx-auto lg:mx-0" />
           <div className="w-full lg:w-2/5">
                 <h1 className="font-Gotham text-headerColorOne font-extrabold text-6xl text-600 mb-8 text-center lg:text-left mt-20 lg:mt-0 lg:text-7xl">EXTENDED EXCOM</h1>
-                <p className="text-black font-circularStd font-semibold text-lg lg:text-lg xl:text-lg">In the dynamic world of ACM DevDay, the Extended Executive Committee (2023-2024) embodies proactive leadership and effective collaboration. As trailblazers in their fields, their transformative influence promises a lasting legacy, inspiring excellence in others. Cheers to an exceptional tenure!</p>
+                <p className="text-black font-circularStd font-semibold mx-4 text-lg lg:text-lg xl:text-lg">In the dynamic world of ACM DevDay, the Extended Executive Committee (2023-2024) embodies proactive leadership and effective collaboration. As trailblazers in their fields, their transformative influence promises a lasting legacy, inspiring excellence in others. Cheers to an exceptional tenure!</p>
             </div>
         </div>
 
