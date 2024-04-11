909da7a8b968d43943102297143067bbffde7709
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2024-04-11 15:56:52 +0500

    oopsie

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HOCHeader from './Components/Header'
 import Navbar from './Components/Navbar'
 import HOCSponsors from './Components/Sponsors'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
