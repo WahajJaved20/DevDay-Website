227449e466886ad40c348534055904176b1fc70b
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2024-04-14 23:19:00 +0500

    page hiweght fix

--- src/Components/AmbassadorsPage.jsx
+++ src/Components/AmbassadorsPage.jsx
@@ -13,7 +13,7 @@ function AmbassadorsPage(){
                 backgroundRepeat: "no-repeat"
             }}
         >
-            <h1 className="text-[15px] md:text-[40px] lg:text-[60px] font-Gotham  font-bold text-headerColorTwo mt-8 lg:mt-0">AMBASSADOR REGISTRATIONS</h1>
+            <h1 className="text-[15px] md:text-[40px] lg:text-[60px] font-Gotham  font-bold text-headerColorTwo mt-2">AMBASSADOR REGISTRATIONS</h1>
             <h1 className="text-[15px] md:text-[40px] lg:text-[60px] font-Gotham font-bold text-headerColorTwo">ARE NOW COMPLETE!</h1>
             <h1 className="text-[15px] lg:text-[25px] font-circularStd font-bold text-headerColorTwo">THANK YOU FOR JOINING US!</h1>
         </div>
